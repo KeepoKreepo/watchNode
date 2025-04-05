ac99bda1d8eb7ec4166538191bc315a25571498e
KeepoKreepo <Oliverspollen0@gmail.com>
2025-04-05 17:19:30 +0100

    Update server.js Fixed added fields v2

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get("/watches", async (req, res) => {
 
 // Add a new watch
 app.post("/watches", async (req, res) => {
-    const { brand, model, type, movement, price_range, water_resistance_m, features, use_case, images } = req.body;
+    const { brand, model, type, movement, case_material, bracelet_material, price_range, water_resistance_m, diameter, features, use_case, images } = req.body;
 
     if (!brand || !model || !type || !movement || !price_range || !water_resistance_m || !features || !use_case) {
         return res.status(400).json({ message: "All fields are required" });
